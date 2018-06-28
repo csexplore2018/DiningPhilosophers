2b5f925aa4ff35e8a043f7d4e3465f2c8de32203
George Corser <gpcorser@svsu.edu>
2018-06-28 07:08:16 -0400

    updated code prior to day 4 to include liftEvenFork and liftOddFork methods

--- src/diningphilosophers/DiningPhilosophers.java
+++ src/diningphilosophers/DiningPhilosophers.java
@@ -6,6 +6,10 @@
 package diningphilosophers;
 
 /**
+ * The purpose of this program is to demonstrate
+ * the Dining Philosophers problem,
+ * using the simple case of just 2 philosophers
+ * See: https://en.wikipedia.org/wiki/Dining_philosophers_problem
  *
  * @author gpcorser
  */
@@ -14,7 +18,7 @@ public class DiningPhilosophers {
     // boolean array of fork statuses (lifted or not) 
     static boolean forkLifted [] = {false, false};
     
-    // could have just use length of forkLifted array
+    // length of forkLifted array
     static int numberOfForks = 2;
 
     /**
@@ -22,32 +26,62 @@ public class DiningPhilosophers {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    Philosopher aristotle = new Philosopher();
+        
+        Philosopher aristotle = new Philosopher();
         aristotle.name = "Aristotle";
         
         Philosopher socrates = new Philosopher();
         socrates.name = "Socrates";
+        
+        // if each philosophers lifts left fork first,
+        // then neither can ever eat, 
+        // because both forks must be lifted to eat.
+        aristotle.liftLeftFork();
+        socrates.liftLeftFork();
+        aristotle.liftRightFork();
+        socrates.liftRightFork();
+        
+        resetForks();
+        
+        // if each philosophers lifts even fork first,
+        // then one philosopher can eat
+        aristotle.liftEvenFork();
+        socrates.liftEvenFork();
+        aristotle.liftOddFork();
+        socrates.liftOddFork();
+        
+    }
+    public static void resetForks() {
+        forkLifted[0] = false;
+        forkLifted[1] = false;
+        System.out.println();
+        System.out.println("Reset Forks");
+        System.out.println();
     }
 }
 
 class Philosopher {
-    String name = "Plato";
-    int philosopherNumber = 0;
-    static int numberOfPhilosophers = 0;
-    // Note: all Forks are created BEFORE any Philosopher is created
+    String name = "Plato"; // name is always Plato, unless changed
+    int philosopherNumber = 0; 
+    static int numberOfPhilosophers = 0; 
     int leftFork = 0;
     int rightFork = 0;
     boolean rightForkUp = false;
     boolean leftForkUp = false;
+    boolean philosopherIsEven = false;
     
     Philosopher() { // constructor
-        numberOfPhilosophers++;
-        philosopherNumber = numberOfPhilosophers;
         
-        rightFork = philosopherNumber - 1;
-        leftFork = philosopherNumber;
-        if (philosopherNumber == DiningPhilosophers.numberOfForks) 
-            leftFork = 1;
+        // philosophers are numbered from 0 to n-1
+        philosopherNumber = numberOfPhilosophers;
+        // numberOfPhilosophers is n
+        numberOfPhilosophers++; 
+        
+        leftFork = philosopherNumber; // leftFork always == phil. num.
+        rightFork = philosopherNumber + 1;
+        // right fork is zero for highest numbered philosopher
+        if (philosopherNumber == DiningPhilosophers.numberOfForks - 1) 
+            rightFork = 0;
     }
     void eat() {
         System.out.println(name + ": Yum!");
@@ -63,6 +97,8 @@ class Philosopher {
         else {
             DiningPhilosophers.forkLifted[leftFork] = true;
             leftForkUp = true;
+            System.out.println(name + ": Successfully lifted left fork: " 
+                    + leftFork);
         }
     }
     void liftRightFork() {
@@ -73,6 +109,16 @@ class Philosopher {
         else {
             DiningPhilosophers.forkLifted[rightFork] = true;
             rightForkUp = true;
+            System.out.println(name + ": Successfully lifted right fork: " 
+                    + rightFork);
         }
     } 
+    void liftEvenFork() {
+        if(leftFork % 2 == 0) liftLeftFork();
+        else liftRightFork();
+    }
+    void liftOddFork() {
+        if(leftFork % 2 != 0) liftLeftFork();
+        else liftRightFork();
+    }
 }
